045271eea9e00f9d50cb54ad32d8061938b46012
Harshitha <VHARSHITHA.23@ichat.sp.edu.sg>
2024-04-27 13:32:25 +0800

    Updated bmi exercise 1

--- bmi.py
+++ bmi.py
@@ -1,16 +1,34 @@
-def calculate_bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
+def calculate_bmi(height,weight):
+    print("Height = "+ str(height))
+    print("Weight = "+str(weight))
 
-    bmi=weight/(height*height)
-    print("BMI = " + str(bmi))
+    bmi = weight/(height*height)
+    print("BMI is equal to : " + str(bmi))
 
     if bmi<18.5:
         print("Under Weight")
     elif bmi<25:
         print("Normal Weight")
     else:
-        print("Over Weight")
-        
+        [print("Over Weight")]
 
-calculate_bmi(weight=57, height=1.73)
+
+calculate_bmi(weight=57 ,height = 1.73)
+
+
+
+
+
+
+
+
+
+
+
+'''alternatibe way to use wihtout identifying string
+
+def calculate_bmi(height,weight):
+    print("Height = ",height)
+    print("Weight = ",weight)
+
+calculate_bmi(weight=57 ,height = 1.73)'''
